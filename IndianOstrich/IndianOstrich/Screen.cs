08bcf7aa03d6cad0090f786bcbf41fd463d58b3e
Paraximia <dabickster@gmail.com>
2014-03-30 18:34:22 -0500

    Adding Screen.cs

---
+++ IndianOstrich/IndianOstrich/Screen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace IndianOstrich
+{
+    abstract class Screen
+    {
+        public Image img { get; set; }
+
+        public void Redirect { }
+
+        public void InOut (Object KeyValue)
+        {
+
+        }
+    }
+}
