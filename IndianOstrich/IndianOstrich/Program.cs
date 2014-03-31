b96dbc4ffd7d8b7b8adbcc6dbe69e1221deb4189
Paraximia <dabickster@gmail.com>
2014-03-30 19:16:49 -0500

    Framework, Main

--- IndianOstrich/IndianOstrich/Program.cs
+++ IndianOstrich/IndianOstrich/Program.cs
@@ -6,5 +6,20 @@ using System.Windows.Forms;
 
 namespace IndianOstrich
 {
-
+    public class IndianOstrich
+    {
+        public static void Main()
+        {
+            //Start
+            int i = 0;
+            while(i < 100)
+            {
+                Console.Write(i++);
+                //infinte loop
+                //Handle event
+            }
+            //End
+        }
+    }
+    
 }
