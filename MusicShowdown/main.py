7312c9b0433927398c35ae01588faf7a00fbc44c
Surya Sharma <surya.sharma233@gmail.com>
2014-04-06 07:23:56 -0500

    Got some movement and camera movements going
    You can move a dude around and it scrolls the background and stuff,
    still have to figure out animations from a sprite sheet and bounding
    the player

--- MusicShowdown/main.py
+++ MusicShowdown/main.py
@@ -1,9 +1,70 @@
-import pygame, sys
-#import has input stuffs
-from pygame.locals.localsimport *
+import pygame
+from player import Player
 
-pygame.init()
-fpsClock = pygame.time.Clock()
+#constants -- using caps and underscores to differentiate them from other vars
+BG_WIDTH = 30000
+BG_HEIGHT = 960
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 768
 
-windowSurfaceObj = pygame.display.set_mode((1024, 768))
-pygame.display.set_caption('best game ever')
+#initialise the camera
+camera = pygame.Rect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
+
+def main():
+	pygame.init()
+	#get the background
+	bg = pygame.image.load("data/level1.png")
+
+	#caption
+	pygame.display.set_caption("This is a game we made it's cool")
+
+	#create screen
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+	#initialise sprites and render it
+	player = Player("data/foo.png") 
+	#renders all the sprites
+	allsprites = pygame.sprite.RenderPlain(player)
+
+
+	#initialise clock
+	clock = pygame.time.Clock()
+
+	running = True
+	while running:
+		clock.tick(60) #60 fps
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				running = False
+			else:
+				player.handleInput(event)
+		setCamera(player)
+		#draw bg
+		screen.blit(bg, (0,0), (camera.x, camera.y, SCREEN_WIDTH, SCREEN_HEIGHT))
+		#update sprites
+		allsprites.update()
+		#draw sprites
+		allsprites.draw(screen)
+		pygame.display.flip()
+
+def setCamera(player):
+	#center it over player
+	camera.x = ( (player.rect.x + player.rect.w)/ 2 ) - ( SCREEN_WIDTH / 2 )
+	camera.y = ( (player.rect.y + player.rect.h)/ 2 ) - ( SCREEN_HEIGHT/ 2 )
+
+	#keep it in bounds
+	if( camera.x < 0 ):
+		camera.x = 0
+
+	if( camera.y < 0 ):
+		camera.y = 0
+
+	if( camera.x > BG_WIDTH - camera.w ):
+		camera.x = LEVEL_WIDTH - camera.w
+
+	if( camera.y > BG_HEIGHT - camera.h ):
+		camera.y = BG_HEIGHT - camera.h
+
+#run the main function if this file is run
+if __name__=="__main__":
+	main()
---
+++ MusicShowdown/player.py
@@ -0,0 +1,42 @@
+import pygame
+
+class Player(pygame.sprite.Sprite):
+	def __init__(self, imagepath):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.image.load(imagepath)
+		self.rect = self.image.get_rect()
+		self.xVel = 0
+		self.yVel = 0
+
+	def update(self):
+		self.move()
+
+	def move(self):
+		self.rect.x += self.xVel
+		self.rect.y += self.yVel
+
+
+	def handleInput(self, event):
+		#deal with left events
+		if( event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT ):
+			self.xVel -= self.rect.w/4
+		elif( event.type == pygame.KEYUP and event.key == pygame.K_LEFT ):
+			self.xVel += self.rect.w/4
+
+		#deal with right events
+		if( event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT ):
+			self.xVel += self.rect.w/4
+		elif( event.type == pygame.KEYUP and event.key == pygame.K_RIGHT ):
+			self.xVel -= self.rect.w/4
+
+		#deal with up events
+		if( event.type == pygame.KEYDOWN and event.key == pygame.K_UP ):
+			self.yVel -= self.rect.w/4
+		elif( event.type == pygame.KEYUP and event.key == pygame.K_UP ):
+			self.yVel += self.rect.w/4
+
+		#deal with down events
+		if( event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN ):
+			self.yVel += self.rect.w/4
+		elif( event.type == pygame.KEYUP and event.key == pygame.K_DOWN ):
+			self.yVel -= self.rect.w/4
