2d20e9aea8d8e1a8fd744d1726f85040274301e3
Surya Sharma <surya.sharma233@gmail.com>
2014-04-07 01:22:52 -0500

    Player animations
    Got the animations working

--- MusicShowdown/player.py
+++ MusicShowdown/player.py
@@ -1,19 +1,90 @@
 import pygame
 
 class Player(pygame.sprite.Sprite):
-	def __init__(self, imagepath):
+	def __init__(self, imagepath, playerW, playerH):
 		pygame.sprite.Sprite.__init__(self)
-		self.image = pygame.image.load(imagepath)
+		self.sheet = pygame.image.load(imagepath)
+		self.playerW = playerW
+		self.playerH = playerH
+
+		#initial image
+		self.sheet.set_clip( pygame.Rect(0, 0, playerW, playerH) )
+		self.image = self.sheet.subsurface( self.sheet.get_clip() )
 		self.rect = self.image.get_rect()
+
+		#velocitites
 		self.xVel = 0
 		self.yVel = 0
 
+		#left = l or right = r
+		self.status = 'r'
+		
+		#current frame
+		self.frame = 0
+		self.leftWalk = []
+		self.rightWalk = []
+		self.getClips()
+
 	def update(self):
+		#if it's moving left 
+		if( self.xVel < 0 ):
+			#change status to left
+			self.status = 'l'
+			#go to next frame
+			self.frame += 1
+		elif( self.xVel > 0 ):
+			#change status to right
+			self.status = 'r'
+			#go to next frame
+			self.frame +=1
+		#if standing
+		else:
+			#reset the animation
+			self.frame = 0
+		#looping
+		if( self.frame >= 4):
+			self.frame = 0
+
+		#check status and change image
+		if( self.status == 'r' ):
+			self.image = self.rightWalk[self.frame]
+		elif( self.status == 'l' ):
+			self.image = self.leftWalk[self.frame]
 		self.move()
 
 	def move(self):
 		self.rect.x += self.xVel
 		self.rect.y += self.yVel
+
+	def getClips(self):
+		#all the rightwalks
+		self.sheet.set_clip( pygame.Rect(0, 0, self.playerW, self.playerH) )
+		self.rightWalk.append( self.sheet.subsurface(self.sheet.get_clip()) )
+
+		self.sheet.set_clip( pygame.Rect( self.playerW, 0, self.playerW, self.playerH) )
+		self.rightWalk.append( self.sheet.subsurface(self.sheet.get_clip()) )
+
+		self.sheet.set_clip( pygame.Rect( self.playerW*2, 0, self.playerW, self.playerH) )
+		self.rightWalk.append( self.sheet.subsurface(self.sheet.get_clip()) )
+
+		self.sheet.set_clip( pygame.Rect( self.playerW*3, 0, self.playerW, self.playerH) )
+		self.rightWalk.append( self.sheet.subsurface(self.sheet.get_clip()) )
+
+
+		#get all the leftwalks
+		self.sheet.set_clip( pygame.Rect(0, self.playerH, self.playerW, self.playerH) )
+		self.leftWalk.append( self.sheet.subsurface(self.sheet.get_clip()) )
+
+		self.sheet.set_clip( pygame.Rect( self.playerW, self.playerH, self.playerW, self.playerH) )
+		self.leftWalk.append( self.sheet.subsurface(self.sheet.get_clip()) )
+
+		self.sheet.set_clip( pygame.Rect( self.playerW*2, self.playerH, self.playerW, self.playerH) )
+		self.leftWalk.append( self.sheet.subsurface(self.sheet.get_clip()) )
+
+		self.sheet.set_clip( pygame.Rect( self.playerW*3, self.playerH, self.playerW, self.playerH) )
+		self.leftWalk.append( self.sheet.subsurface(self.sheet.get_clip()) )
+
+
 
 
 	def handleInput(self, event):
@@ -35,8 +106,6 @@ class Player(pygame.sprite.Sprite):
 		elif( event.type == pygame.KEYUP and event.key == pygame.K_UP ):
 			self.yVel += self.rect.w/4
 
-			#down events should be crouch not downward movement
-			
 		#deal with down events
 		if( event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN ):
 			self.yVel += self.rect.w/4
