8dcf20c61fd4d9dd22452e15c88898ee8e2a0fef
Th3MiteeyLambo <th3miteeylambo@hotmail.com>
2014-04-08 03:01:15 -0500

    committing again

--- MusicShowdown/prop.py
+++ MusicShowdown/prop.py
@@ -1,10 +1,18 @@
 import pygame
 
 class Prop(pygame.sprite.Sprite):
-	def __init__(self, imagepath):
+	def __init__(self, imagepath, levelW, levelH, spawnPoint):
 		pygame.sprite.Sprite.__init__(self)
 		self.image = pygame.image.load(imagepath)
 		self.rect = self.image.get_rect()
-		self.rect.x = 2000
 		self.xVel = 0
 		self.yVel = 0
+		#self.kind = kind
+		self.levelW = levelW
+		self.levelH = levelH
+		self.propW = self.rect.w
+		self.propH = self.rect.h
+		self.spawnPoint = spawnPoint
+
+		self.rect.x = spawnPoint
+		self.rect.y = levelH - self.propW - 64
